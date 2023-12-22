235b051ef2bd595c2a342e921d9589397d70a5d6
Jorge Revuelta <jrjrevuelta@gmail.com>
2023-12-22 13:59:18 +0100

    Las putas letras dan problema

---
+++ app.js
@@ -0,0 +1,60 @@
+import { elements, englishWords } from './domReferences.js';
+
+
+document.querySelector('#start-game').addEventListener('click', getMandatoryInput);
+
+function getMandatoryInput(){
+  const {usernameInput, playerName, playGame}= elements;
+  const usernameValue = usernameInput.value;
+  if(!usernameValue){
+      alert("ingresa username");
+  } else {
+      showForm(playerName, playGame);
+      createLetters();
+      chooseWord();
+  }
+}
+
+
+
+
+
+
+
+function chooseWord() {
+  const {wordContainer} = elements;
+  const randomNumber = Math.floor(Math.random() * 130);
+  const chosenWord = englishWords[randomNumber];
+  for (let i = 0; i < chosenWord.length; i++) {
+    const underlineLetter = document.createElement('div');
+    underlineLetter.classList.add('underlineLetter');
+    underlineLetter.textContent = '';
+    wordContainer.appendChild(underlineLetter);
+  }
+  console.log(chosenWord);
+}
+
+
+function showForm(sectiontoHide, sectiontoShow){
+  sectiontoHide.classList.add("hide");
+  sectiontoShow.classList.remove("hide");
+}
+
+function createLetters() {
+  const {lettersContainer} = elements;
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  for (let i = 0; i < letters.length; i++) {
+    const letter = document.createElement('button');
+    letter.classList.add('letter');
+    letter.textContent = letters[i];
+    lettersContainer.appendChild(letter);
+    let isTrue = true;
+    letter.addEventListener('click', () => {
+      if (isTrue) {
+        console.log(letter);
+        letter.classList.add('hidden');
+        isTrue = false;
+      }
+    });
+  }
+}
---
+++ domReferences.js
@@ -0,0 +1,34 @@
+export const elements = {
+  gameDisplay: document.querySelector('.play-game'),   
+  usernameInput: document.querySelector("#input-username"),
+  playerName: document.getElementById("player-name"),
+  playGame: document.querySelector(".play-game"),
+  clickHereBtn: document.getElementById("click-here-btn"),
+  gameDisplayBtn: document.getElementById("game-display"),
+  yourScore: document.getElementById("your-score"),
+  totalClicks: document.getElementById("total-clicks"),
+  userScores: document.getElementById("user-scores"),
+  lettersContainer: document.querySelector('.letters-container'),
+  wordContainer: document.querySelector('.word')
+};
+
+
+export const englishWords = [
+  "apple", "banana", "chocolate", "dolphin", "elephant", "freedom", "guitar", "happiness",
+  "internet", "jazz", "kangaroo", "lighthouse", "moonlight", "navigate", "opportunity", "paradise",
+  "quasar", "rainbow", "serendipity", "tangerine", "umbrella", "vibrant", "whisper", "xylophone",
+  "yellow", "zeppelin", "amazing", "blossom", "cascade", "delight", "effervescent", "fantastic",
+  "glisten", "harmony", "inspire", "jubilant", "kaleidoscope", "lullaby", "magnificent", "nostalgia",
+  "oasis", "paradox", "quintessential", "radiant", "serenity", "tranquil", "uplifting", "verdant",
+  "wonder", "xylograph", "yesterday", "zenith", "adventure", "breathtaking", "captivating", "dreamscape",
+  "eloquent", "fascinate", "gratitude", "harmonious", "innovation", "juxtapose", "kismet", "labyrinth",
+  "mellifluous", "nirvana", "optimistic", "petrichor", "quixotic", "rhapsody", "symbiosis", "tranquility",
+  "ubiquitous", "vivid", "whimsical", "xanadu", "yearning", "zephyr", "ambrosial", "beatitude", "cynosure",
+  "denouement", "epiphany", "felicity", "gossamer", "halcyon", "idyllic", "jocund", "kaleidoscopic", "luminescent",
+  "mirth", "noble", "oscillate", "panacea", "quandary", "resplendent", "seraphic", "talisman", "ubiquity",
+  "voluptuous", "whistle", "xenial", "yearning", "zestful", "alacrity", "benevolent", "comely", "divulge",
+  "effulgent", "felicitous", "gormandize", "halcyon", "insouciant", "jubilant", "kaleidoscopic", "lagniappe",
+  "mellifluous", "nonpareil", "obfuscate", "paragon", "quiescent", "resplendent", "salubrious", "taciturn",
+  "ubiquitous", "veritable", "wherewithal", "xanadu", "yesterday", "zephyr",
+  // ... Add more words as needed
+];
