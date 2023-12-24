35c82d7da1bde1b23c6740be102b0ad5bae05808
Jorge Revuelta <jrjrevuelta@gmail.com>
2023-12-24 13:05:54 +0100

    win/lose done

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ function chooseWord() {
   const {wordContainer} = elements;
   const randomNumber = Math.floor(Math.random() * 130);
   const chosenWord = englishWords[randomNumber];
+  //////variables to determine if hangman is complete or word is complete
+  let fails = 0;
+  let success = 0;
   for (let i = 0; i < chosenWord.length; i++) {
     const underlineLetter = document.createElement('div');
     underlineLetter.classList.add('underlineLetter');
@@ -25,7 +28,7 @@ function chooseWord() {
     wordContainer.appendChild(underlineLetter);
   }
   console.log(chosenWord);
-  createLetters(chosenWord);
+  createLetters(chosenWord, fails, success);
 }
 
 
@@ -34,7 +37,7 @@ function showForm(sectiontoHide, sectiontoShow){
   sectiontoShow.classList.remove("hide");
 }
 
-function createLetters(chosenWord) {
+function createLetters(chosenWord, fails, success) {
   const {lettersContainer} = elements;
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   /////creation of letter DIVS
@@ -62,18 +65,19 @@ function createLetters(chosenWord) {
             if (letterPositions.includes(i)) {
               element.textContent = letter.textContent;
               element.classList.add('spotted');
+              success++;
             }
           })
+          if (success === chosenWord.length) alert('You win')
           ///////Still need to figure out how to check that all have the class spotted to declare winner.
         } else {
           //////Access the DOM of the parts of the hangman
           const {hangman} = elements;
           const hangmanParts = hangman.querySelectorAll('div');
-
-          console.log('Nope');
+          fails++;
+          hangmanParts[fails].classList.remove(`hide-${fails}`);
+          if (fails === 9) alert('You lost')
         }
-
-
         letter.classList.add('hidden');
         isTrue = false;
       }
